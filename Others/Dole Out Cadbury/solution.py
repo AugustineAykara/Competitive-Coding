3d4ad4d2d81a1bf8f84f0bf65b0a1e1991f55630
Augustine S Aykara <augustineaykara@gmail.com>
2020-06-22 00:01:40 +0530

    Update solution.py

--- Others/Dole Out Cadbury/solution.py
+++ Others/Dole Out Cadbury/solution.py
@@ -1,20 +1,16 @@
 def countSquares(a, b):
     minSide = min(a, b)
     maxSide = max(a, b)
-    pair = (minSide, maxSide)
 
     if(minSide == 0):
         return(0)
     if(minSide == 1):
         return(minSide * maxSide)
 
-
     count = maxSide // minSide
     newSide = maxSide % minSide
 
     count += countSquares(minSide, newSide)
-    dictionary[pair] = count
-
     return(count)
 
 p = int(input())
@@ -22,7 +18,6 @@ q = int(input())
 r = int(input())
 s = int(input())
 
-dictionary = {}
 res = 0
 for i in range(p, q+1):
     for j in range(r, s+1):
