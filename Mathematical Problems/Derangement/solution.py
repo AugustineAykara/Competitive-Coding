a48c60e3e877d03f67eca86ef0bf808dd3112175
Augustine S Aykara <augustineaykara@gmail.com>
2020-07-04 20:32:10 +0530

    Update solution.py

--- Mathematical Problems/Derangement/solution.py
+++ Mathematical Problems/Derangement/solution.py
@@ -4,14 +4,15 @@ def derangements(n):
         return(0)
     if(n == 0 or n == 2):
         return(1)
-
-    d = [0 for i in range(0, n+1)]
+    
     d[0] = 1
     d[1] = 0
     d[2] = 1
     for i in range(3, n+1):
         d[i] = (i-1)*(d[i-1] + d[i-2])
+    
     return(d[i])
 
 n = int(input())
+d = [0 for i in range(0, n+1)]
 print(derangements(n))
