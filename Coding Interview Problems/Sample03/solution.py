b5dc159deaf043f74d749df977d3ebc452b00fb9
Augustine S Aykara <augustineaykara@gmail.com>
2020-07-23 21:38:13 +0530

    Create solution.py

---
+++ Coding Interview Problems/Sample03/solution.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+
+    if(memo[n] != 0):
+        return(memo[n])
+    elif (n == 0):
+        return(0)
+    elif(n == 1 or n == 2):
+        return(1)
+    else:
+        memo[n] = fibonacci(n-1) + fibonacci(n-2)
+        return(memo[n])
+
+n = int(input())
+memo = [0] * (n+1)
+print(fibonacci(n))
