59da0c07b33ec333aa1a1a65b049ffb9c89668be
Augustine S Aykara <augustineaykara@gmail.com>
2020-10-07 11:15:51 +0530

    nth fibonacci term using Memoization

--- Coding Interview Problems/Sample03/solution.py
+++ Coding Interview Problems/Sample03/solution.py
@@ -1,15 +1,14 @@
-def fibonacci(n):
-
-    if (memo[n] != 0):
-        return (memo[n])
-    elif (n == 0):
-        return (0)
-    elif (n == 1 or n == 2):
-        return (1)
-    else:
-        memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        return (memo[n])
+def fib(n):
+    
+    if(n in memo):
+        return(memo[n]) 
+   
+    if(n == 1 or n == 0):
+        return memo[n]
+    
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
 
 n = int(input())
-memo = [0] * (n)
-print(fibonacci(n-1))
+memo = {0:0, 1:1}
+print(fib(n-1))
